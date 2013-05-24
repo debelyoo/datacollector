0d970dd48917c46c0f3787093f94a61fcddb5c1a
Jean Rossier <jean.rossier@gmail.com>
2013-05-24 17:16:50 +0200

    clea nup

--- src/main/scala/ecol/cassandra/model/Sensor.scala
+++ src/main/scala/ecol/cassandra/model/Sensor.scala
@@ -1,13 +1,6 @@
 package ecol.cassandra.model
 
-import com.netflix.astyanax.model.ColumnFamily
-import java.util.UUID
-import com.netflix.astyanax.serializers._
-import ecol.cassandra.AstyanaxConnector
-import spray.json.DefaultJsonProtocol
-import spray.json.RootJsonFormat
 import spray.json._
-import com.netflix.astyanax.connectionpool.exceptions.ConnectionException
 
 /**
  * The object representing a sensor connected to the Compactrio
